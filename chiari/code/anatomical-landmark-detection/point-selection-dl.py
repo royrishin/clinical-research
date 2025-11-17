eed18f0b521deec21753821c6697cbab03d58111
iam.rishinroy@gmail.com <iam.rishinroy@gmail.com>
2025-11-16 21:18:29 -0600

    added code design diagram

--- chiari/code/anatomical-landmark-detection/point-selection-dl.py
+++ chiari/code/anatomical-landmark-detection/point-selection-dl.py
@@ -1,2 +1,2 @@
 # Here I will be working on creating code for using deep learning to automatically detect 
-anatomical landmark in images such bastion and opisthion coordinates in image. 
+#anatomical landmark in images such bastion and opisthion coordinates in image. 
