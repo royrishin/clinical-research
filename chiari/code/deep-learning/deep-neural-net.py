851c942b6f572448c7d5bb10f4d78f6afcf98747
royrishin <iam.rishinroy@gmail.com>
2025-12-07 22:41:28 -0600

    Add deep learning model insights for Chiari detection
    Document findings on using deep learning for Chiari malformation detection in MRI scans, including model performance metrics.

---
+++ chiari/code/deep-learning/deep-neural-net.py
@@ -0,0 +1,13 @@
+#https://pmc.ncbi.nlm.nih.gov/articles/PMC9271110/
+
+
+# In search of the classification ML model that was trained as part of this research that gave potentially positive results from the tests.
+
+Through further studies I am finding it would be better to use AI driven approach to use deep learning model to detect the chiari malformation in the images. 
+It seems in the above research program deep learning neural net CNN (concolutional neural network) model was trained with classification technique on MRI scan images to assist clinicians in disgnosis & decision making of chari malfunction. 
+Results captured are as below:
+
+Results
+The VGG19 model (VGG19 is a 19-layer deep convolutional neural network architecture that is widely used in computer vision tasks like image classification, feature extraction, and neural style transfer) with data 
+augmentation achieved a sensitivity of 97.1% and a specificity of 97.4% with an AUC of 0.99. The ResNet50 (convolutional neural network (CNN) that excels at image classification) model achieved a sensitivity of 94.0% and a specificity of 94.4% with an AUC of 0.98.
+
